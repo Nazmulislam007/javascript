dde3b8b0af96f660337f981e8527ecb6367662ee
nazmul <nazmulislam.ni897@gmail.com>
2021-12-30 17:28:53 +0600

    beginning

---
+++ app.js
@@ -0,0 +1,11 @@
+// ✅✅check the browser console
+// console.log("hello word");
+
+// ✅✅all documents are converts to "hello world"
+// document.write("hello word");
+
+// ✅✅print the whole document
+// window.print();
+
+// ✅✅alert something
+// alert("hello world");
