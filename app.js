2a506d038802ea50ef43aa38d36cb68691be502d
Nazmul Islam Nahid <86213586+Nazmulislam007@users.noreply.github.com>
2022-01-11 14:43:19 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // ✅✅check the browser console
-// console.log("hello word");
+console.log("hello word");
 
 // ✅✅all documents are converts to "hello world"
-// document.write("hello word");
+document.write("hello word");
 
 // ✅✅print the whole document
-// window.print();
+window.print();
 
 // ✅✅alert something
-// alert("hello world");
+alert("hello world");
