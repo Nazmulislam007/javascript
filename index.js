64b5b809123893293e9add4b1a82cd86d7a4b333
Nazmul Islam Nahid <86213586+Nazmulislam007@users.noreply.github.com>
2022-02-18 19:34:27 +0600

    add mm/dd/yyyy

--- index.js
+++ index.js
@@ -34,3 +34,20 @@ if (hour === 0 && prepand === "AM") {
 }
 
 console.log(`The Time: ${hour} ${prepand} : ${minute} : ${second}`);
+
+
+const date = new Date();
+let dd = date.getDate();
+let mm = date.getMonth();
+const yy = date.getFullYear();
+
+if (dd < 10) {
+  dd = "0" + dd;
+}
+
+if (mm < 10) {
+  mm = "0" + mm;
+}
+
+const today = `${dd} - ${mm} - ${yy}`;
+console.log(today);
