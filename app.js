ceb2421e159bd2d635caa419f7e5f1d82af4fec2
nazmul <nazmulislam.ni897@gmail.com>
2022-02-18 15:56:09 +0600

    time zone

--- app.js
+++
@@ -1,9 +0,0 @@
-// statement:
-const demo = document.getElementById("demo"); //this is a statement;
-
-let a, b, c;
-a = 5;
-b = 6;
-c = a + b;
-
-demo.innerHTML = c;
---
+++ scripts.js
@@ -0,0 +1,35 @@
+const today = new Date();
+
+const day = today.getDay();
+
+const dayList = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday ",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const todayIs = `Today is : ${dayList[day]}`;
+console.log(todayIs);
+
+const minute = today.getMinutes();
+const second = today.getSeconds();
+let hour = today.getHours();
+
+let prepand = hour >= 12 ? "PM" : "AM";
+hour = hour >= 12 ? hour - 12 : hour;
+
+if (hour === 0 && prepand === "PM") {
+  hour = 12;
+  prepand = "PM";
+}
+
+if (hour === 0 && prepand === "AM") {
+  hour = 12;
+  prepand = "AM";
+}
+
+console.log(`The Time: ${hour} ${prepand} : ${minute} : ${second}`);
