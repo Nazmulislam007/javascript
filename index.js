73cf5bc5f584cb544415bebd96db975823ae681b
Nazmul Islam Nahid <86213586+Nazmulislam007@users.noreply.github.com>
2022-02-22 17:49:05 +0600

    Update index.js

--- index.js
+++ index.js
@@ -30,3 +30,15 @@ leapYears(2016);
 leapYears(2015);
 // console.log(leapYears(2014));
 // console.log(leapYears(2012));
+
+
+// find 1st January is being a Sunday between 2014 and 2050.
+
+for (let year = 2014; year <= 2050; year++) {
+  const time = new Date(year, 0, 1);
+  if (time.getDay() === 0) {
+    console.log(`first january is being sunday ${year}`);
+  } else {
+    console.log("false");
+  }
+}
