cc6cb169868730592bd2973c578aa1ce2450a09a
nazmul <nazmulislam.ni897@gmail.com>
2022-02-22 18:54:32 +0600

    prompt match number

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const leapYear = (year) => {
-  if ((year % 100 === 0 && year % 4 === 0) || year % 400 === 0) {
-    // return console.log(`this ${year} is leap year`);
-  } else {
-    // return console.log(`this ${year} is not leap year`);
-  }
-};
+// todo problem: 06 find the leapyear
+// const leapYear = (year) => {
+//   if ((year % 100 === 0 && year % 4 === 0) || year % 400 === 0) {
+//     return console.log(`this ${year} is leap year`);
+//   } else {
+//     return console.log(`this ${year} is not leap year`);
+//   }
+// };
 
-leapYear(1400);
+// leapYear(1400);
 
-const leapYear2 = (year) => {
-  return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
-};
+// const leapYear2 = (year) => {
+//   return year % 100 === 0 ? year % 400 === 0 : year % 4 === 0;
+// };
 
 // console.log(leapYear2(2010));
 // console.log(leapYear2(2011));
@@ -18,27 +19,38 @@ const leapYear2 = (year) => {
 // console.log(leapYear2(2013));
 // console.log(leapYear2(2048));
 
-const leapYears = (year) => {
-  const time = new Date(year, 1, 29);
-  if (time.getMonth() === 1) {
-    console.log("this is leap year");
-  } else {
-    console.log("this " + year + " not a leap year");
-  }
-};
-leapYears(2016);
-leapYears(2015);
+// const leapYears = (year) => {
+//   const time = new Date(year, 1, 29);
+//   if (time.getMonth() === 1) {
+//     console.log("this is leap year");
+//   } else {
+//     console.log("this " + year + " not a leap year");
+//   }
+// };
+// leapYears(2016);
+// leapYears(2015);
 // console.log(leapYears(2014));
 // console.log(leapYears(2012));
 
+// todo problem:7. find 1st January is being a Sunday between 2014 and 2050.
 
-// find 1st January is being a Sunday between 2014 and 2050.
+// for (let year = 2014; year <= 2050; year++) {
+//   const time = new Date(year, 0, 1);
+//   if (time.getDay() === 0) {
+//     console.log(`first january is being sunday ${year}`);
+//   } else {
+//     console.log("false");
+//   }
+// }
 
-for (let year = 2014; year <= 2050; year++) {
-  const time = new Date(year, 0, 1);
-  if (time.getDay() === 0) {
-    //console.log(`first january is being sunday ${year}`);
-  } else {
-    //console.log("false"); 
-  }
+//todo problem:8. Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched"
+
+const num = Math.ceil(Math.random() * 10);
+
+const gnum = prompt("Guess the number between 1 to 10");
+
+if (gnum == num) {
+  alert("Great job");
+} else {
+  alert("Oops the number is " + num + "");
 }
