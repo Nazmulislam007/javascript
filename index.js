c118ae964aaf5d50b71a8e5942f82ecf8147ae28
nazmul <nazmulislam.ni897@gmail.com>
2022-02-22 14:28:32 +0600

    childNodes[0] and substring

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = document.getElementById("target");
+const textNode = element.childNodes[0];
+let text = textNode.data;
+
+// console.log(text[text.length - 1] + text.substring(0, text.length - 1));
+
+window.addEventListener("load", () => {
+  setInterval(() => {
+    text = text[text.length - 1] + text.substring(0, text.length - 1);
+    // console.log(text);
+    textNode.data = text;
+  }, 100);
+});
