c001f917deb7cb858a82fa1bbe13068aafe9afc0
Nazmul Islam Nahid <86213586+Nazmulislam007@users.noreply.github.com>
2022-02-22 14:41:28 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,53 +1,14 @@
-const today = new Date();
-// checking date
+const element = document.getElementById("target");
+const textNode = element.childNodes[0];
+let text = textNode.data;
 
-const day = today.getDay();
+// console.log(text[text.length - 1] + text.substring(0, text.length - 1));
 
-const dayList = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday ",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+window.addEventListener("load", () => {
+  setInterval(() => {
+    text = text[text.length - 1] + text.substring(0, text.length - 1);
+    // console.log(text);
+    textNode.data = text;
+  }, 100);
+});
 
-const todayIs = `Today is : ${dayList[day]}`;
-console.log(todayIs);
-
-const minute = today.getMinutes();
-const second = today.getSeconds();
-let hour = today.getHours();
-
-let prepand = hour >= 12 ? "PM" : "AM";
-hour = hour >= 12 ? hour - 12 : hour;
-
-if (hour === 0 && prepand === "PM") {
-  hour = 12;
-  prepand = "PM";
-}
-
-if (hour === 0 && prepand === "AM") {
-  hour = 12;
-  prepand = "AM";
-}
-
-console.log(`The Time: ${hour} ${prepand} : ${minute} : ${second}`);
-
-
-const date = new Date();
-let dd = date.getDate();
-let mm = date.getMonth();
-const yy = date.getFullYear();
-
-if (dd < 10) {
-  dd = "0" + dd;
-}
-
-if (mm < 10) {
-  mm = "0" + mm;
-}
-
-const today = `${dd} - ${mm} - ${yy}`;
-console.log(today);
